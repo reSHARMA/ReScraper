d34dae4f454d79e3c595d9ac7bd411ba5bd9f310
reSHARMA <reshabhsh@gmail.com>
2017-02-20 17:57:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/scrape', function(req, res){
+
+url = 'http://www.imdb.com/title/tt1229340/';
+
+request(url, function(error, response, html){
+    if(!error){
+        var $ = cheerio.load(html);
+
+    var title, release, rating;
+    var json = { title : "", release : "", rating : ""};
+
+    
+
+    
+
+
+})
+
+// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+res.send('Check your console!')
+
+    }) ;
+})
+
+app.listen('8081')
+console.log('Magic happens on port 8081');
